7604dd7e31519698dd035eca336cc81f2bb408e4
yusrinelson <125978974+yusrinelson@users.noreply.github.com>
2023-05-04 15:08:53 +0200

    added eventlisteners code

--- IWA_18_4/scripts.js
+++ IWA_18_4/scripts.js
@@ -29,11 +29,53 @@ const handleDragOver = (event) => {
 
 
 const handleDragStart = (event) => {}
+
 const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
+
+const handleHelpToggle = (event) => {
+    const { target } = event;
+    if (target == html.other.help) {
+        html.help.overlay.showModal();
+    }
+    if (target == html.help.cancel) {
+        html.help.overlay.close();
+    }
+}
+const handleAddToggle = (event) => {
+    const {target} = event
+    if (target == html.other.add) {
+        html.add.overlay.showModal()
+    }
+    if(target == html.add.cancel){
+        html.add.overlay.close()
+    }
+}
+const handleAddSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+        title: html.add.title.value,
+        table: html.add.table.value,
+        column: html.edit.column.value || html
+    }
+    const props = createOrderData(data)
+    const content = createOrderHtml(props)
+    const orderedColumn = document.querySelector('[data-area="ordered"]');
+    const orderedDiv = orderedColumn.querySelector('[data-column="ordered"]');
+    orderedDiv.appendChild(content)
+    handleAddToggle();
+
+}
+const handleEditToggle = (event) => {
+    const {target} = event
+    const opened = document.querySelector(".order")
+    if (target == opened){
+        html.edit.overlay.showModal()
+    }
+    if(target == html.edit.cancel){
+        html.edit.overlay.close()
+    }
+}
+
 const handleEditSubmit = (event) => {}
 const handleDelete = (event) => {}
 
